61e0d7c3aadb0bbde161c19f2d2bf99d8c8c25b2
Hyunsu Cho <phcho@nvidia.com>
2025-12-12 15:51:55 -0800

    Update formatter config

--- cpp/scripts/run-cmake-format.sh
+++ cpp/scripts/run-cmake-format.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 # This script is a wrapper for cmakelang that may be used with pre-commit. The
 # wrapping is necessary because RAPIDS libraries split configuration for
 # cmakelang linters between a local config file and a second config file that's
-# shared across all of RAPIDS via rapids-cmake.
+# shared across all of RAPIDS via rapids-cmake. In order to keep it up to date
+# this file is only maintained in one place (the rapids-cmake repo) and
+# pulled down during builds. We need a way to invoke CMake linting commands
+# without causing pre-commit failures (which could block local commits or CI),
+# while also being sufficiently flexible to allow users to maintain the config
+# file independently of a build directory.
+#
+# This script provides the minimal functionality to enable those use cases. It
+# searches in a number of predefined locations for the rapids-cmake config file
+# and exits gracefully if the file is not found. If a user wishes to specify a
+# config file at a nonstandard location, they may do so by setting the
+# environment variable RAPIDS_CMAKE_FORMAT_FILE.
+#
+# This script can be invoked directly anywhere within the project repository.
+# Alternatively, it may be invoked as a pre-commit hook via
+# `pre-commit run (cmake-format)|(cmake-lint)`.
+#
+# Usage:
+# bash run-cmake-format.sh {cmake-format,cmake-lint} infile [infile ...]
 
 status=0
-if [ -z ${CUFOREST_ROOT:+PLACEHOLDER} ]; then
-    CUFOREST_BUILD_DIR=$(git rev-parse --show-toplevel 2>&1)/cpp/build
+if [ -z ${CUML_ROOT:+PLACEHOLDER} ]; then
+    CUML_BUILD_DIR=$(git rev-parse --show-toplevel 2>&1)/cpp/build
     status=$?
 else
-    CUFOREST_BUILD_DIR=${CUFOREST_ROOT}
+    CUML_BUILD_DIR=${CUML_ROOT}
 fi
 
 if ! [ ${status} -eq 0 ]; then
-    if [[ ${CUFOREST_BUILD_DIR} == *"not a git repository"* ]]; then
-        echo "This script must be run inside the cuforest repository, or the CUFOREST_ROOT environment variable must be set."
+    if [[ ${CUML_BUILD_DIR} == *"not a git repository"* ]]; then
+        echo "This script must be run inside the raft repository, or the RAFT_ROOT environment variable must be set."
     else
         echo "Script failed with unknown error attempting to determine project root:"
-        echo "${CUFOREST_BUILD_DIR}"
+        echo "${CUML_BUILD_DIR}"
     fi
     exit 1
 fi
 
 DEFAULT_FORMAT_FILE_LOCATIONS=(
-  "${CUFOREST_BUILD_DIR:-${HOME}}/_deps/rapids-cmake-src/cmake-format-rapids-cmake.json"
+  "${CUML_BUILD_DIR:-${HOME}}/_deps/rapids-cmake-src/cmake-format-rapids-cmake.json"
 )
 
 if [ -z ${RAPIDS_CMAKE_FORMAT_FILE:+PLACEHOLDER} ]; then
@@ -53,6 +71,9 @@ fi
 if [[ $1 == "cmake-format" ]]; then
   cmake-format -i --config-files cpp/cmake/config.json "${RAPIDS_CMAKE_FORMAT_FILE}" -- "${@:2}"
 elif [[ $1 == "cmake-lint" ]]; then
+  # Since the pre-commit hook is verbose, we have to be careful to only
+  # present cmake-lint's output (which is quite verbose) if we actually
+  # observe a failure.
   OUTPUT=$(cmake-lint --config-files cpp/cmake/config.json "${RAPIDS_CMAKE_FORMAT_FILE}" -- "${@:2}")
   status=$?
 
@@ -61,4 +82,3 @@ elif [[ $1 == "cmake-lint" ]]; then
   fi
   exit ${status}
 fi
-
