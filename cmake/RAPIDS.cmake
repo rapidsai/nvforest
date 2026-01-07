35209edcac75a40b484542596363ce628bea2fd9
James Lamb <jlamb@nvidia.com>
2026-01-07 10:53:54 -0600

    apply cmake formatting changes

--- cmake/RAPIDS.cmake
+++ cmake/RAPIDS.cmake
@@ -1,15 +1,15 @@
 # =============================================================================
-# Copyright (c) 2021-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2026, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-# in compliance with the License. You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software distributed under the License
-# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-# or implied. See the License for the specific language governing permissions and limitations under
-# the License.
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 # =============================================================================
 #
 # This is the preferred entry point for projects using rapids-cmake
@@ -19,9 +19,7 @@ cmake_minimum_required(VERSION 3.30.4 FATAL_ERROR)
 
 # Allow users to control which version is used
 if(NOT (rapids-cmake-branch OR rapids-cmake-version))
-  message(
-    FATAL_ERROR "The CMake variable `rapids-cmake-branch` or `rapids-cmake-version` must be defined"
-  )
+  message(FATAL_ERROR "The CMake variable `rapids-cmake-branch` or `rapids-cmake-version` must be defined")
 endif()
 
 # Allow users to control which GitHub repo is fetched
@@ -79,8 +77,7 @@ else()
 endif()
 FetchContent_GetProperties(rapids-cmake)
 if(rapids-cmake_POPULATED)
-  # Something else has already populated rapids-cmake, only thing we need to do is setup the
-  # CMAKE_MODULE_PATH
+  # Something else has already populated rapids-cmake, only thing we need to do is setup the CMAKE_MODULE_PATH
   if(NOT "${rapids-cmake-dir}" IN_LIST CMAKE_MODULE_PATH)
     list(APPEND CMAKE_MODULE_PATH "${rapids-cmake-dir}")
   endif()
