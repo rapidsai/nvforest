dc46eb65c8ea4faf41e9f61cfef35cdb6caf77a1
Gil Forsyth <gforsyth@nvidia.com>
2026-02-10 16:40:12 -0500

    feat: build `cuforest` conda package using limited API

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -14,7 +14,7 @@ conda config --set channel_priority strict
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-github python)
+PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name conda_python cuforest --stable --cuda)")
 
 rapids-logger "Generate Python testing dependencies"
 rapids-dependency-file-generator \
