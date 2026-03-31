4c026b9cfa1932429c7846efac85af83af53ec62
James Lamb <jaylamb20@gmail.com>
2026-03-31 15:47:08 -0500

    remove unused intersphinx configs (#94)
    Yesterday's nightly build on `main` failed like this:
    
    > loading intersphinx inventory 'scipy' from https://docs.scipy.org/doc/scipy/objects.inv ...
    loading intersphinx inventory 'sklearn' from https://scikit-learn.org/stable/objects.inv ...
    loading intersphinx inventory 'treelite' from https://treelite.readthedocs.io/en/latest/objects.inv ...
    WARNING: failed to reach any of the inventories with the following issues:
    intersphinx inventory 'https://docs.scipy.org/doc/scipy/objects.inv' not fetchable due to <class 'requests.exceptions.ConnectTimeout'>: HTTPSConnectionPool(host='docs.scipy.org', port=443): Max retries exceeded with url: /doc/scipy/objects.inv (Caused by ConnectTimeoutError(<HTTPSConnection(host='docs.scipy.org', port=443) at 0x7a5853ca11d0>, 'Connection to docs.scipy.org timed out. (connect timeout=None)'))
    building [mo]: targets for 0 po files that are out of date
    
    ([build link](https://github.com/rapidsai/nvforest/actions/runs/23783492846/job/69301772677))
    
    Because of docs-hosting issues with scipy:
    
    * https://github.com/scipy/scipy/issues/24950
    * https://github.com/scipy/docs.scipy.org/issues/102
    
    This PR removes `intersphinx` configuration for `cudf` and `scipy`... nvForest doesn't use either, and removing those reduces the risk of similar failures in the future.
    
    Similar to https://github.com/rapidsai/cuml/pull/7935
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Jim Crist-Harif (https://github.com/jcrist)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/nvforest/pull/94

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -203,10 +203,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "cudf": ("https://docs.rapids.ai/api/cudf/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "python": ("https://docs.python.org/3", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy", None),
     "sklearn": ("https://scikit-learn.org/stable/", None),
     "treelite": ("https://treelite.readthedocs.io/en/latest/", None),
 }
