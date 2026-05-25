4cb6fe5e4b493218ea233eaef2f59e52fa76b473
Hyunsu Cho <phcho@nvidia.com>
2026-05-24 20:19:45 -0700

    Rename libnvforest++ -> libnvforest

--- ci/build_wheel_nvforest.sh
+++ ci/build_wheel_nvforest.sh
@@ -20,7 +20,7 @@ LIBNVFOREST_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libnvforest_${RAPIDS_PY_CUDA_SUFF
 echo "libnvforest-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBNVFOREST_WHEELHOUSE}"/libnvforest_*.whl)" >> "${PIP_CONSTRAINT}"
 
 EXCLUDE_ARGS=(
-  --exclude "libnvforest++.so"
+  --exclude "libnvforest.so"
   --exclude "libraft.so"
   --exclude "libcublas.so.*"
   --exclude "libcublasLt.so.*"
